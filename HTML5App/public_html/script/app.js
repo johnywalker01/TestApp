c97b1f7f801d3f6e30fea42fbe6ddbc5d1d22626
1014821 <1014821@D1574.quest-global.com>
2016-08-18 18:43:11 +0530

    3rd check-in 3 functionalities:- string in camel-case; string in reverse format; string in alternate uppercase.

--- HTML5App/public_html/script/app.js
+++ HTML5App/public_html/script/app.js
@@ -2,8 +2,13 @@
 
 var myApp = angular.module('myApp', []);
 
-myApp.controller("myController", function () {
+myApp.controller("myController", function ($filter) {
   this.userName = "";
+  this.text = "";
+  this.camelCase = function(){
+    this.text = $filter('camelCase')(this.text);
+    alert(this.text);
+  };
 });
 
 myApp.filter('reverse', function () {
@@ -22,6 +27,21 @@ myApp.filter('alternateUppercase', function () {
     for (i = 0; i < x.length; i++) {
       c = x[i];
       if (i % 2 == 0) {
+        c = c.toUpperCase();
+      }
+      txt += c;
+    }
+    return txt;
+  };
+});
+myApp.filter('camelCase', function () {
+  return function (x) {
+    var newX = x.toString().trim();
+    var i, c, txt = "";
+    for (i = 0; i < newX.length; i++) {
+      c = x[i];
+      if (c == " ") {
+        c=x[++i];
         c = c.toUpperCase();
       }
       txt += c;
