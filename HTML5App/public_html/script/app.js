f879b8e04b3b1a81817f125591e5d2994bc6517a
1014821 <1014821@D1574.quest-global.com>
2016-08-18 18:17:22 +0530

    2nd check-in

--- HTML5App/public_html/script/app.js
+++ HTML5App/public_html/script/app.js
@@ -1,8 +1,32 @@
 // create the module and name it myApp
 
-var myApp = angular.module('myApp',[]);
+var myApp = angular.module('myApp', []);
 
 myApp.controller("myController", function () {
   this.userName = "";
+});
+
+myApp.filter('reverse', function () {
+  return function (x) {
+    var i, c, txt = "";
+    for (i = (x.length - 1); i >= 0; i--) {
+      c = x[i];
+      txt += c;
+    }
+    return txt;
+  };
+});
+myApp.filter('alternateUppercase', function () {
+  return function (x) {
+    var i, c, txt = "";
+    for (i = 0; i < x.length; i++) {
+      c = x[i];
+      if (i % 2 == 0) {
+        c = c.toUpperCase();
+      }
+      txt += c;
+    }
+    return txt;
+  };
 });
 
