56c0d74b84d821f3d1aacb67142f63733455cc4b
1014821 <1014821@D1574.quest-global.com>
2016-08-30 16:55:38 +0530

    updated 'camelcase' method removal of unnecessary spaces.

--- HTML5App/public_html/script/app.js
+++ HTML5App/public_html/script/app.js
@@ -2,7 +2,7 @@
 
 var myApp = angular.module('myApp', []);
 
-myApp.controller("myController", function ($filter) {
+myApp.controller("myController", function ($scope,$filter) {
   this.string = "";
   this.resultString = "";
   this.convertToReverse = function(){
@@ -11,9 +11,53 @@ myApp.controller("myController", function ($filter) {
   this.convertToAlternateUppercase = function(){
     this.resultString = $filter('alternateUppercase')(this.string);
   };
-  this.convertToCamelCase = function(){
-    this.resultString = $filter('camelCase')(this.string);
+  this.convertToCamelCase = function () {
+    this.resultString = $filter('camelCase')(this.string.trim());
   };
+  this.myTime = Date();
+  this.foo1 = function () {
+    this.myTime = Date();
+    alert(this.myTime);
+    this.myTime = setTimeout($scope.foos, 5000);
+    alert("this should happen after 5 seconds");
+  };
+  $scope.foos = function () {
+    $scope.newTime = Date();
+    alert($scope.newTime);
+    return $scope.newTime = Date();
+  };
+  this.foo2 = function () {
+    setTimeout("alert('this alert is timedout and should be the first');", 5000);
+    alert("this should be the second one");
+  };
+  this.test = function (flag) {
+    if (!flag) {
+      this.myTime = setTimeout(function () {alert("flag - "+flag);return "ABC";}, 2500);
+    }
+    // code that you cannot modify
+  };
+
+  this.workerValue;
+  $scope.w;
+  this.startWorker = function () {
+    if (typeof (Worker) !== "undefined") {
+      if (typeof ($scope.w) == "undefined") {
+        $scope.w = new Worker("script/demo_workers.js");
+        $scope.onmessage = function (event) {
+          console.log("value-"+event.data);
+          this.workerValue = event.data;
+        };
+      }
+    } else {
+      this.workerValue = "Sorry, your browser does not support Web Workers...";
+    }
+  };
+
+  this.stopWorker = function () {
+    $scope.w.terminate();
+    $scope.w = undefined;
+  };
+
 });
 
 myApp.filter('reverse', function () {
@@ -46,7 +90,11 @@ myApp.filter('camelCase', function () {
     for (i = 0; i < newX.length; i++) {
       c = x[i];
       if (c == " ") {
-        c=x[++i];
+        c = x[++i];
+        if(c == " "){
+          --i;
+          continue;
+        }
         c = c.toUpperCase();
       }
       txt += c;
